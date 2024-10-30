2f2115864eb9366a3affc67de72552e1f58a58f7
h-rsh-n <harshanp1514@gmail.com>
2024-10-30 16:12:12 +0530

    Added proxy for the services

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,5 +6,7 @@ module.exports = {
   PORT:process.env.PORT,
   SALT_ROUNDS:process.env.SALT_ROUNDS,
   JWT_SECRET:process.env.JWT_SECRET,
-  EXPIRES_IN:process.env.EXPIRES_IN
+  EXPIRES_IN:process.env.EXPIRES_IN,
+  FLIGHT_SERVICE:process.env.FLIGHT_SERVICE,
+  BOOKING_SERVICE:process.env.BOOKING_SERVICE
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const {serverConfig,loggerConfig} = require('./config')
 const app = express();
 const apiRouter = require('./routes')
+const {rateLimit} = require('express-rate-limit')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const limiter = rateLimit({
+  windowMs:2*60*1000,
+  limit:10
+})
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+app.use(limiter)
+app.use('/flightService',createProxyMiddleware({target:serverConfig.FLIGHT_SERVICE,changeOrigin:true,pathRewrite:{'^/flightService':'/'}}))
+app.use('/bookingService',createProxyMiddleware({target:serverConfig.BOOKING_SERVICE,changeOrigin:true,pathRewrite:{'^/bookingService':'/'}}))
 app.use('/api',apiRouter);
 
 app.listen(serverConfig.PORT,()=>{
